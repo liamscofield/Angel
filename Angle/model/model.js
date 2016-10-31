61813e4c3e7dee78bd6f485991fd4d0c052fb278
morning <morning@thecover.cn>
2016-10-31 15:38:28 +0800

    依赖库安装

---
+++ Angle/model/model.js
@@ -0,0 +1,68 @@
+var mongoose = require('mongoose');
+var uuid_generator = require('uuid');
+mongoose.connect('mongodb://localhost/angle');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console," connection error:"));
+db.once('open', function(){
+	//we're connected;
+})
+
+var userSchema = mongoose.Schema({
+	name: String,
+	password: String,
+	uid: String
+})
+
+var User = mongoose.model('User', userSchema);
+
+function createUser(username, password, callback) {
+	User.findOne({name: username}, function(err, user){
+		if (err) {
+			console.log('find user error' + err);
+			createuserAndSaveToDB(username, password, function(error, user){
+				if (error) {
+					callback(error);
+				} else {
+					callback(null);
+				}
+			});
+		} else {
+			if (user != null) {
+				callback("user existed");
+			} else {
+
+				createuserAndSaveToDB(username, password, function(error, user){
+				if (error) {
+					callback(error);
+				} else {
+					callback(null);
+				}
+			});
+			}
+		};
+	});
+}
+
+function createuserAndSaveToDB(username, password,callback) {
+	var user = new User({name: username, password: password, uid: uuid_generator.v4()});
+	user.save(function(err, user) {
+		if (err) {
+			callback(err,null);
+		} else {
+			callback(null, user);
+		}
+	});
+}
+
+function findSomebody(username, callback) {
+	User.findOne({name: username}, function(err, user){
+		if(err) return callback(err, nil);
+		return callback(null,user);
+	});
+}
+
+
+
+exports.createUser = createUser;
+exports.findSomebody = findSomebody;
